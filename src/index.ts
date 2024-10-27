226cd41b11454fa8b7b8a7c2c48eb7dbd2dfc4cc
gburning <7988867+gburning@users.noreply.github.com>
2024-10-27 21:56:07 +0100

    feat: add basic nunjucks support

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,8 @@ export const languages: SupportLanguage[] = [
 	{
 		name: "JinjaTemplate",
 		parsers: [PLUGIN_KEY],
-		extensions: [".jinja", ".jinja2", ".j2", ".html"],
-		vscodeLanguageIds: ["jinja"],
+		extensions: [".jinja", ".jinja2", ".j2", ".html", ".njk"],
+		vscodeLanguageIds: ["jinja", "nunjucks", "njk"],
 	},
 ];
 
